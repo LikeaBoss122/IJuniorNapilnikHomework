7cc905c5b9595a7c07086761736b4abf0e1557eb
Quddi <alemkhf12@gmail.com>
2021-07-06 22:12:58 +0300

    a

---
+++ 1_1.cs
@@ -0,0 +1,39 @@
+﻿class Weapon
+    {
+        private int Damage;
+        readonly int Bullets;
+
+        public bool TryFire(Player player)
+        {
+            if (Bullets > 0)
+            {
+                player.GetDamage(Damage);
+                Bullets -= 1;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    class Player
+    {
+        readonly int Health;
+
+        public void GetDamage(int Damage)
+        {
+            Health -= Damage;
+        }
+    }
+
+    class Bot
+    {
+        private Weapon Weapon;
+
+        public void OnSeePlayer(Player player)
+        {
+            Weapon.TryFire(player);
+        }
+    }
