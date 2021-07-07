2a8b0c5b6db0282f08d5b1652d76c50fdf224020
Quddi <59485777+LikeaBoss122@users.noreply.github.com>
2021-07-07 17:08:53 +0300

    Update Weapon.cs

--- 1_1/Weapon.cs
+++ 1_1/Weapon.cs
@@ -10,57 +10,45 @@ class Weapon
 
     public Weapon(int damageStart, int bulletsStartCount, int maxMagazineBullets, int maxCurrentBullets)
     {
-        if (damageStart > 0 && bulletsStartCount >= 0)
-        {
-            _damage = damageStart;
-            _maxCurrentBullets = maxCurrentBullets;
-            _maxMagazineBullets = maxMagazineBullets;
-            CurrentBulletsCount = Math.Min(maxCurrentBullets, bulletsStartCount);
-            MagazineBulletsCount = maxCurrentBullets > bulletsStartCount ? 0 : bulletsStartCount - maxCurrentBullets;
-        }
-        else
-        {
+        if (damageStart <= 0 || bulletsStartCount < 0)
             throw new InvalidOperationException();
-        }
+        
+        _damage = damageStart;
+        _maxCurrentBullets = maxCurrentBullets;
+        _maxMagazineBullets = maxMagazineBullets;
+        CurrentBulletsCount = Math.Min(maxCurrentBullets, bulletsStartCount);
+        MagazineBulletsCount = maxCurrentBullets > bulletsStartCount ? 0 : bulletsStartCount - maxCurrentBullets;
     }
 
     public bool TryFire(Player player)
     {
-        if (player != null)
-        {
-            if (_enoughBullets)
-            {
-                player.GetDamage(_damage);
-                CurrentBulletsCount -= 1;
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        else throw new InvalidOperationException();
+        if (player == null)
+            throw new InvalidOperationException();
+        
+        if(!_enoughBullets)
+            return false;
+
+        player.TakeDamage(_damage);
+        CurrentBulletsCount -= 1;
+        return true;
     }
 
     private bool TryReload()
     {
-        if (MagazineBulletsCount > 0 && CurrentBulletsCount != _maxCurrentBullets)
+        if (MagazineBulletsCount <= 0 || CurrentBulletsCount == _maxCurrentBullets)
+            return false;
+            
+        if (_maxCurrentBullets < _allBulletsCount)
         {
-            if (_maxCurrentBullets < _allBulletsCount)
-            {
-                MagazineBulletsCount = _allBulletsCount - _maxCurrentBullets;
-                CurrentBulletsCount = _maxCurrentBullets;
-            }
-            else
-            {
-                CurrentBulletsCount = _allBulletsCount;
-                MagazineBulletsCount = 0;
-            }
-            return true;
+            MagazineBulletsCount = _allBulletsCount - _maxCurrentBullets;
+            CurrentBulletsCount = _maxCurrentBullets;
         }
         else
         {
-            return false;
+            CurrentBulletsCount = _allBulletsCount;
+            MagazineBulletsCount = 0;
         }
+        
+        return true;
     }
 }
